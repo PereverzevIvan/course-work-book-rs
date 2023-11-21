b7234d9bd058c97110ce6310c1ba29fc5bc060d6
PereverzevIvan <74914699+PereverzevIvan@users.noreply.github.com>
2023-11-21 21:38:13 +0300

    install django rest-framework

--- book_rs/book_rs/settings.py
+++ book_rs/book_rs/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
