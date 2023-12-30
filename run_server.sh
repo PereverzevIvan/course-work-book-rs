120c84d080ff004bf3b0354f81bc656337f1bc04
PereverzevIvan <peregh320@gmail.com>
2023-12-30 09:09:29 +0300

    Добавлен файл requirements.txt

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
-source '/home/ivan/Работа/Polytech/Курсовая работа/course-work-book-rs/venv/bin/activate'
-python '/home/ivan/Работа/Polytech/Курсовая работа/course-work-book-rs/book_rs/manage.py' runserver
+source './venv/bin/activate'
+python './book_rs/manage.py' runserver
