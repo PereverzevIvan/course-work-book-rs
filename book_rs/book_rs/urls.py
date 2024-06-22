41680494ed72539e22976541aa13a6a2b84ab8bd
PereverzevIvan <peregh320@gmail.com>
2024-06-22 23:17:45 +0300

    Добавлен swagger

--- book_rs/book_rs/urls.py
+++ book_rs/book_rs/urls.py
@@ -19,8 +19,26 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+SchemaView = get_schema_view(
+ openapi.Info(
+ title="Snippets API",
+ default_version='v1',
+ description="Test description",
+ terms_of_service="https://www.google.com/policies/terms/",
+ contact=openapi.Contact(email="contact@snippets.local"),
+ license=openapi.License(name="BSD License"),
+ ),
+ public=True,
+ permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path("", include("RecommendSystem.urls")),
     path("users/", include("users.urls")),
     path("admin/", admin.site.urls),
+    path('swagger/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
